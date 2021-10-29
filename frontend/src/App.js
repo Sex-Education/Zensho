45d197fd42202d5d35b776c39aaa4c6acb8cb557
iluvyw <anhoangpham2001@gmail.com>
2021-10-30 00:58:16 +0700

    Update Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import LoginPage from './Pages/LoginPage/LoginPage';
+import Image from '../src/assets/login-background.jpg'
 
 function App() {
   return (
-    <div className="App">
+    <div className="relative overflow-y-hidden h-screen w-screen bg-red-400 flex flex-row items-center justify-center">
+      <img className="absolute z-0 w-full" src={Image} alt="background"/>
       <LoginPage/>
     </div>
   );
