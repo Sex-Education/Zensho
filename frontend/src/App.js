91b0a696710a362f4972b85d6302fe97019b00d3
An Pham <anhoangpham2001@gmail.com>
2022-01-07 16:35:00 +0700

    Browse all recently dataset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import UploadDataset from './pages/UploadDataset';
 import StoreOverview from './pages/StoreOverview';
 import ViewDataset from './pages/ViewDataset';
 import Search from './pages/Search';
+import BrowseDataset from './pages/BrowseDataset';
+
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { useState } from 'react';
 import AuthContext from './context/auth.context';
@@ -27,6 +29,7 @@ function App() {
               <Route path='/dataset/:id' element={<><NavBar /><ViewDataset /></>} />
               <Route path='/search' element={<><NavBar /><Search /></>} />
               <Route path='/subscription' element={<><NavBar /></>} />
+              <Route path='/browse' element={<><NavBar /><BrowseDataset /></>} />
               <Route
                 path="*"
                 element={<Navigate to="/store" />}
---
+++ frontend/src/components/BrowseItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function BrowseItem({id, imgSrc, datasetName, username, updatedDate, categories}){
+    const navigate = useNavigate()
+    
+    const navigateDataset = (id) => {
+        navigate(`/dataset/${id}`)
+    }
+    
+    return (
+        <div className="h-80 bg-transparent overflow-hidden p-4 rounded-2xl border-2 border-white cursor-pointer" onClick={() => navigateDataset(id)}>
+            <img src={imgSrc} alt="dataset" className="h-1/2 w-full object-cover"/>
+            <div className="flex flex-col">
+                <h1 className="text-2xl text-yellow-400 font-bold mb-4">{datasetName}</h1>
+                <h2 className="text-sm font-light">by <span className="underline font-bold">{username}</span></h2>
+                <h3 className="text-sm font-light">Updated at {updatedDate}</h3>
+                <div className="flex flex-row">
+                    {categories.map(item => <h3 className="mr-3 bg-yellow-500 py-1 px-2 rounded-2xl">{item}</h3>)}
+                </div>
+            </div>
+        </div>
+    )
+}
