a96c1fced2979e6d90e4b4446fca0472cd32fd92
ngoccuongpvc <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>
2022-01-07 12:33:15 +0700

    no bug please

--- app/models/dataset.go
+++ app/models/dataset.go
@@ -19,7 +19,7 @@ type Dataset struct {
 
 func (d *Dataset) Save() error {
 	statement := `INSERT INTO dataset (dataset_name, username, dataset_url, description, image_url) values($1,$2,$3,$4,$5)`
-	_, e := connection.PostgresConnection.Exec(statement, d.DatasetName, d.UserName, d.DatasetUrl, d.ImageUrl)
+	_, e := connection.PostgresConnection.Exec(statement, d.DatasetName, d.UserName, d.DatasetUrl, d.Description, d.ImageUrl)
 	if e != nil {
 		// log.Fatal(e.Error())
 		return e
