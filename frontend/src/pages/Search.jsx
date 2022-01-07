e629b553eadf0ed5670f8a90a8746d2c008a4f1e
iluvyw <anhoangpham2001@gmail.com>
2022-01-07 21:11:31 +0700

    fix dataset length

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -9,48 +9,35 @@ To the right we have an upvote button with current vote count, and also the down
 below which we show how many files are contained in the dataset and the total size of them all.
 The result will not be paginated, instead we allow the user to scroll endlessly, loading more and more search result as they scroll down more to the bottom.
 */
-
-import React, { useEffect } from "react";
-import SearchNav from "../components/SearchNav";
-import SearchItem from "../components/SearchItem";
-import { useSearchParams } from "react-router-dom";
-import axios from "axios";
-import { useState } from "react/cjs/react.development";
+import React, { useEffect } from 'react'
+import SearchNav from '../components/SearchNav'
+import SearchItem from '../components/SearchItem'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react/cjs/react.development'
 
 export default function Search() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [datasetList, setDatasetList] = useState([]);
+    const [searchParams,setSearchParams] = useSearchParams()
+    const [datasetList,setDatasetList] = useState([])
 
-  const q = searchParams.get("q");
+    const q = searchParams.get('q')
 
-  useEffect(() => {
-    axios
-      .get(`https://zensho.herokuapp.com/api/dataset?name=${q}&page=1&limit=10`)
-      .then((response) => {
-        setDatasetList(response.data.data);
-        console.log("search", response.data.data);
-      });
-  }, [q]);
-
-  return (
-    <div className="ml-64 page-width h-full background-gray text-white flex flex-col items-center overflow-y-scroll">
-      <SearchNav />
-      <div className="w-11/12 border-b border-white mt-4">
-        <h1>
-          {datasetList.length} results for "{q}"
-        </h1>
-      </div>
-      {datasetList.map((item) => (
-        <SearchItem
-          key={item.id}
-          id={item.id}
-          imageSrc={item.imageSrc}
-          datasetName={item.name}
-          username={item.username}
-          description={item.description}
-          categories={["Dog", "Cat"]}
-        />
-      ))}
-    </div>
-  );
+    useEffect(() => {
+        axios.get(`https://zensho.herokuapp.com/api/dataset?name=${q}&page=1&limit=10`).then(
+            (response) => {
+                setDatasetList(response.data.data)
+                console.log("search",response.data.data)
+            }
+        )
+    },[q])
+    
+    return (
+        <div className="ml-64 page-width h-full background-gray text-white flex flex-col items-center overflow-y-scroll">
+            <SearchNav/>
+            <div className="w-11/12 border-b border-white mt-4">
+                <h1>{datasetList.length} results for "{q}"</h1>
+            </div>
+            {datasetList.map(item => <SearchItem key={item.id} id={item.id} imageSrc={item.imageSrc} datasetName={item.name} username={item.username} description={item.description} categories={["Dog","Cat"]}/>)}
+        </div>
+    )
 }
